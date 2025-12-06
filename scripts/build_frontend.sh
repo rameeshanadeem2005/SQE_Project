b81bb7e10c600ac28d9a4f2dd2ec608f6e6db565
EC2 Default User <ec2-user@ip-172-31-33-35.ap-northeast-3.compute.internal>
2025-12-06 21:14:01 +0000

    Updated build_frontend

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/idurar-erp-crm/frontend
+
+echo "Building frontend..."
 npm run build
-cp -r dist/* ../backend/public/
+
+rm -rf /home/ec2-user/idurar-erp-crm/backend/src/public/*
+cp -r dist/* /home/ec2-user/idurar-erp-crm/backend/src/public/
--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pm2 stop all || true
-cd /home/ec2-user/idurar-erp-crm/backend
+cd /home/ec2-user/idurar-erp-crm/backend/src
 pm2 start server.js --name "staging-backend"
